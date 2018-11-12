8f8ff6252b9b3fef566170d7c0e548fbc22f4830
Johnny C. Lam <jlam88@gmail.com>
2018-11-12 11:37:36 -0600

    Initial import of MooSpec-1.0.
    MooSpec-1.0 is a library that provides information about player
    specializations and roles.

---
+++ MooSpec-1.0.lua
@@ -0,0 +1,469 @@
+--[[--------------------------------------------------------------------
+    Copyright (C) 2018 Johnny C. Lam.
+    See the file LICENSE.txt for copying permission.
+--]]--------------------------------------------------------------------
+
+-- GLOBALS: assert
+-- GLOBALS: LibStub
+
+local MAJOR, MINOR = "MooSpec-1.0", 1
+assert(LibStub, MAJOR .. " requires LibStub")
+assert(LibStub("CallbackHandler-1.0", true), MAJOR .. " requires CallbackHandler-1.0")
+assert(LibStub("MooInspect-1.0", true), MAJOR .. " requires MooInspect-1.0")
+assert(LibStub("MooUnit-1.0", true), MAJOR .. " requires MooUnit-1.0")
+local lib, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
+if not lib then return end
+
+------------------------------------------------------------------------
+
+local format = string.format
+local next = next
+local pairs = pairs
+local setmetatable = setmetatable
+local strfind = string.find
+local strjoin = strjoin
+local strmatch = string.match
+local tonumber = tonumber
+local tostring = tostring
+local tostringall = tostringall
+local type = type
+local wipe = wipe
+-- GLOBALS: _G
+-- GLOBALS: GetAddOnMetadata
+local FONT_COLOR_CODE_CLOSE = FONT_COLOR_CODE_CLOSE
+local GREEN_FONT_COLOR_CODE = GREEN_FONT_COLOR_CODE
+local NORMAL_FONT_COLOR_CODE = NORMAL_FONT_COLOR_CODE
+
+local MooInspect = LibStub("MooInspect-1.0")
+local MooUnit = LibStub("MooUnit-1.0")
+
+--[[--------------------------------------------------------------------
+    Debugging code from LibResInfo-1.0 by Phanx.
+    https://github.com/Phanx/LibResInfo
+--]]--------------------------------------------------------------------
+
+local isAddon = GetAddOnMetadata(MAJOR, "Version")
+
+local DEBUG_LEVEL = isAddon and 2 or 0
+local DEBUG_FRAME = ChatFrame3
+
+local function debug(level, text, ...)
+	if level <= DEBUG_LEVEL then
+		if ... then
+			if type(text) == "string" and strfind(text, "%%[dfqsx%d%.]") then
+				text = format(text, ...)
+			else
+				text = strjoin(" ", tostringall(text, ...))
+			end
+		else
+			text = tostring(text)
+		end
+		DEBUG_FRAME:AddMessage(GREEN_FONT_COLOR_CODE .. MAJOR .. FONT_COLOR_CODE_CLOSE .. " " .. text)
+	end
+end
+
+if isAddon then
+	-- GLOBALS: SLASH_MOOSPEC1
+	-- GLOBALS: SlashCmdList
+	SLASH_MOOSPEC1 = "/moospec"
+	SlashCmdList.MOOSPEC = function(input)
+		input = tostring(input or "")
+
+		local CURRENT_CHAT_FRAME
+		for i = 1, 10 do
+			local cf = _G["ChatFrame"..i]
+			if cf and cf:IsVisible() then
+				CURRENT_CHAT_FRAME = cf
+				break
+			end
+		end
+
+		local of = DEBUG_FRAME
+		DEBUG_FRAME = CURRENT_CHAT_FRAME
+
+		if strmatch(input, "^%s*[0-9]%s*$") then
+			local v = tonumber(input)
+			debug(0, "Debug level set to", input)
+			DEBUG_LEVEL = v
+			DEBUG_FRAME = of
+			return
+		end
+
+		local f = _G[input]
+		if type(f) == "table" and type(f.AddMessage) == "function" then
+			debug(0, "Debug frame set to", input)
+			DEBUG_FRAME = f
+			return
+		end
+
+		debug(0, "Version " .. MINOR .. " loaded. Usage:")
+		debug(0, format("%s%s %s%s - change debug verbosity, valid range is 0-6",
+			NORMAL_FONT_COLOR_CODE, SLASH_MOOSPEC1, DEBUG_LEVEL, FONT_COLOR_CODE_CLOSE))
+		debug(0, format("%s%s %s%s -- change debug output frame",
+			NORMAL_FONT_COLOR_CODE, SLASH_MOOSPEC1, of:GetName(), FONT_COLOR_CODE_CLOSE))
+
+		DEBUG_FRAME = of
+	end
+end
+
+------------------------------------------------------------------------
+
+lib.callbacks = lib.callbacks or LibStub("CallbackHandler-1.0"):New(lib)
+lib.callbacksInUse = lib.callbacksInUse or {}
+
+local eventFrame = lib.eventFrame or CreateFrame("Frame")
+lib.eventFrame = eventFrame
+eventFrame:UnregisterAllEvents()
+
+local function OnEvent(frame, event, ...)
+	return frame[event] and frame[event](frame, event, ...)
+end
+
+eventFrame:SetScript("OnEvent", OnEvent)
+
+function lib.callbacks:OnUsed(lib, callback)
+	if not next(lib.callbacksInUse) then
+		debug(1, "Callbacks in use! Starting up...")
+		eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
+		eventFrame:RegisterEvent("PLAYER_SPECIALIZATION_CHANGED")
+		eventFrame:RegisterEvent("ROLE_CHANGED_INFORM")
+		MooInspect.RegisterCallback(eventFrame, "MooInspect_InspectReady", "OnInspectReady")
+		MooUnit.RegisterCallback(eventFrame, "MooUnit_UnitChanged", "OnUnitChanged")
+		MooUnit.RegisterCallback(eventFrame, "MooUnit_UnitJoined", "OnUnitJoined")
+		MooUnit.RegisterCallback(eventFrame, "MooUnit_UnitLeft", "OnUnitLeft")
+	end
+	lib.callbacksInUse[callback] = true
+end
+
+function lib.callbacks:OnUnused(lib, callback)
+	lib.callbacksInUse[callback] = nil
+	if not next(lib.callbacksInUse) then
+		debug(1, "No callbacks in use. Shutting down...")
+		eventFrame:UnregisterAllEvents()
+		MooInspect.UnregisterCallback(eventFrame, "MooInspect_InspectReady")
+		MooUnit.UnregisterCallback(eventFrame, "MooUnit_UnitChanged")
+		MooUnit.UnregisterCallback(eventFrame, "MooUnit_UnitJoined")
+		MooUnit.UnregisterCallback(eventFrame, "MooUnit_UnitLeft")
+	end
+end
+
+------------------------------------------------------------------------
+
+-- GLOBALS: GetInspectSpecialization
+-- GLOBALS: UnitClass
+-- GLOBALS: UnitGroupRolesAssigned
+-- GLOBALS: UnitIsPlayer
+
+-- Preserve mappings across library upgrades.
+local blizzardRoleByGUID = lib.blizzardRoleByGUID or {}
+local roleByGUID = lib.roleByGUID or {}
+local specializationByGUID = lib.specializationByGUID or {}
+
+lib.blizzardRoleByGUID = blizzardRoleByGUID
+lib.roleByGUID = roleByGUID
+lib.specializationByGUID = specializationByGUID
+
+-- Default roles for each class.
+local roleByClass = {
+	DEATHKNIGHT = "melee",
+	DEMONHUNTER = "melee",
+	DRUID = "ranged",
+	HUNTER = "ranged",
+	MAGE = "ranged",
+	MONK = "melee",
+	PALADIN = "melee",
+	PRIEST = "ranged",
+	ROGUE = "melee",
+	SHAMAN = "ranged",
+	WARLOCK = "ranged",
+	WARRIOR = "melee",
+}
+
+-- Map return values from GetInspectSpecialization() to roles.
+-- ref: https://www.wowpedia.org/API_GetInspectSpecialization
+local roleBySpecialization = {
+	-- Death Knight
+	[250] = "tank", -- Blood
+	[251] = "melee", -- Frost
+	[252] = "melee", -- Unholy
+	-- Demon Hunter
+	[577] = "melee", -- Havoc
+	[581] = "tank", -- Vengeance
+	-- Druid
+	[102] = "ranged", -- Balance
+	[103] = "melee", -- Feral
+	[104] = "tank", -- Guardian
+	[105] = "healer", -- Restoration
+	-- Hunter
+	[253] = "ranged", -- Beast Mastery
+	[254] = "ranged", -- Marksmanship
+	[255] = "melee", -- Survival
+	-- Mage
+	[62] = "ranged", -- Arcane
+	[63] = "ranged", -- Fire
+	[64] = "ranged", -- Frost
+	-- Monk
+	[268] = "tank", -- Brewmaster
+	[270] = "healer", -- Mistweaver
+	[269] = "melee", -- Windwalker
+	-- Paladin
+	[65] = "healer", -- "Holy
+	[66] = "tank", -- "Protection
+	[67] = "melee", -- "Retribution
+	-- Priest
+	[256] = "healer", -- Discipline
+	[257] = "healer", -- Holy
+	[258] = "ranged", -- Shadow
+	-- Rogue
+	[259] = "melee", -- Assassination
+	[260] = "melee", -- Outlaw
+	[261] = "melee", -- Subtlety
+	-- Shaman
+	[262] = "ranged", -- Elemental
+	[263] = "melee", -- Enhancement
+	[264] = "healer", -- Restoration
+	-- Warlock
+	[265] = "ranged", -- Affliction
+	[266] = "ranged", -- Demonology
+	[267] = "ranged", -- Destruction
+	-- Warrior
+	[71] = "melee", -- Arms
+	[72] = "melee", -- Fury
+	[73] = "tank", -- Protection
+}
+
+-- Map return values from GetInspectSpecialization() to names.
+local specializationName = {
+	-- Death Knight
+	[250] = "blood",
+	[251] = "frost",
+	[252] = "unholy",
+	-- Demon Hunter
+	[577] = "havoc",
+	[581] = "vengeance",
+	-- Druid
+	[102] = "balance",
+	[103] = "feral",
+	[104] = "guardian",
+	[105] = "restoration",
+	-- Hunter
+	[253] = "beast_mastery",
+	[254] = "marksmanship",
+	[255] = "survival",
+	-- Mage
+	[62] = "arcane",
+	[63] = "fire",
+	[64] = "frost",
+	-- Monk
+	[268] = "brewmaster",
+	[270] = "mistweaver",
+	[269] = "windwalker",
+	-- Paladin
+	[65] = "holy",
+	[66] = "protection",
+	[67] = "restribution",
+	-- Priest
+	[256] = "discipline",
+	[257] = "holy",
+	[258] = "shadow",
+	-- Rogue
+	[259] = "assassination",
+	[260] = "outlaw",
+	[261] = "subtlety",
+	-- Shaman
+	[262] = "elemental",
+	[263] = "enhancement",
+	[264] = "restoration",
+	-- Warlock
+	[265] = "affliction",
+	[266] = "demonology",
+	[267] = "destruction",
+	-- Warrior
+	[71] = "arms",
+	[72] = "fury",
+	[73] = "protection",
+}
+
+local blizzardRoleByRole = {
+	tank = "TANK",
+	healer = "HEALER",
+	melee = "DAMAGER",
+	ranged = "DAMAGER",
+	none = "NONE",
+}
+
+function lib:GetBlizzardRole(guid)
+	if not blizzardRoleByGUID[guid] then
+		local unit = MooUnit:GetUnitByGUID(guid)
+		if unit then
+			blizzardRoleByGUID[guid] = UnitGroupRolesAssigned(unit)
+		end
+	end
+	return blizzardRoleByGUID[guid] or "NONE"
+end
+
+function lib:GetRole(guid)
+	return roleByGUID[guid] or "none"
+end
+
+function lib:GetSpecialization(guid)
+	local specialization = specializationByGUID[guid]
+	if specialization then
+		local name = self:GetSpecializationName(specialization)
+		return specialization, name
+	end
+	return nil
+end
+
+function lib:GetSpecializationName(specialization)
+	return specializationName[specialization]
+end
+
+local function UpdateBlizzardRole(guid, unit, role)
+	local oldRole = blizzardRoleByGUID[guid] or "NONE"
+	if oldRole ~= role then
+		blizzardRoleByGUID[guid] = role
+		debug(2, "MooSpec_UnitBlizzardRoleChanged", guid, unit, oldRole, role)
+		lib.callbacks:Fire("MooSpec_UnitBlizzardRoleChanged", guid, unit, oldRole, role)
+	end
+end
+
+local function UpdateRole(guid, unit, role)
+	local oldRole = lib:GetRole(guid)
+	if oldRole ~= role then
+		roleByGUID[guid] = role
+		debug(2, "MooSpec_UnitRoleChanged", guid, unit, oldRole, role)
+		lib.callbacks:Fire("MooSpec_UnitRoleChanged", guid, unit, oldRole, role)
+	end
+end
+
+local function UpdateSpecialization(guid, unit, specialization)
+	local oldSpecialization = lib:GetSpecialization(guid)
+	if oldSpecialization ~= specialization then
+		specializationByGUID[guid] = specialization
+		debug(2, "MooSpec_UnitSpecializationChanged", guid, unit, oldSpecialization, specialization)
+		lib.callbacks:Fire("MooSpec_UnitSpecializationChanged", guid, unit, oldSpecialization, specialization)
+	end
+end
+
+local function UpdateUnit(guid, unit)
+	unit = unit or MooUnit:GetUnitByGUID(guid)
+	if unit then
+		if UnitIsPlayer(unit) then
+			debug(3, "UpdateUnit", guid, unit)
+			local specialization = GetInspectSpecialization(unit)
+			-- Validate the return value against the table of possible IDs.
+			if not roleBySpecialization[specialization] then
+				local _, class = UnitClass(unit)
+				debug(1, "Unknown player specialization:", guid, class, specialization)
+			end
+			UpdateSpecialization(guid, unit, specialization)
+			local role = roleBySpecialization[specialization]
+			UpdateRole(guid, unit, role)
+			local blizzardRole = lib:GetBlizzardRole(guid)
+			UpdateBlizzardRole(guid, unit, blizzardRole)
+		end
+	else
+		-- GUID no longer maps to a usable unit ID.
+		eventFrame:OnUnitLeft("UpdateUnit", guid)
+	end
+end
+
+------------------------------------------------------------------------
+
+-- GLOBALS: UnitGUID
+-- GLOBALS: UnitIsPlayer
+
+local function OnUnitSpecializationChanged(event, unit)
+	debug(3, "OnUnitSpecializationChanged", event, unit)
+	if UnitIsPlayer(unit) then
+		local guid = UnitGUID(unit)
+		if guid then
+			MooInspect:QueueInspect(guid)
+		end
+	end
+end
+
+function eventFrame:PLAYER_ENTERING_WORLD(event)
+	OnUnitSpecializationChanged(event, "player")
+end
+
+function eventFrame:PLAYER_SPECIALIZATION_CHANGED(event, unit)
+	OnUnitSpecializationChanged(event, unit)
+end
+
+function eventFrame:ROLE_CHANGED_INFORM(event, changedPlayer, changedBy, oldRole, newRole)
+	debug(3, event, changedPlayer, changedBy, oldRole, newRole)
+	local guid = MooUnit:GetGUIDByName(changedPlayer)
+	if guid then
+		local unit = MooUnit:GetUnitByGUID(guid)
+		if unit then
+			UpdateBlizzardRole(guid, unit, newRole)
+		else
+			-- GUID no longer maps to a usable unit ID.
+			self:OnUnitLeft(event, guid)
+		end
+	end
+end
+
+function eventFrame:OnInspectReady(event, guid)
+	debug(3, event, guid)
+	UpdateUnit(guid)
+end
+
+function eventFrame:OnUnitJoined(event, guid, unit)
+	if UnitIsPlayer(unit) then
+		debug(3, "OnUnitJoined", event, guid, unit)
+		-- Set a default role if this unit had no previous role.
+		local role = lib:GetRole(guid)
+		if role == "none" then
+			local _, class = UnitClass(unit)
+			if class then
+				role = roleByClass[class]
+				UpdateRole(guid, unit, role)
+			end
+		end
+		MooInspect:QueueInspect(guid)
+	end
+end
+
+function eventFrame:OnUnitLeft(event, guid)
+	debug(3, "OnUnitLeft", event, guid)
+	MooInspect:CancelInspect(guid)
+	blizzardRoleByGUID[guid] = nil
+	roleByGUID[guid] = nil
+	specializationByGUID[guid] = nil
+end
+
+eventFrame.OnUnitChanged = eventFrame.OnUnitJoined
+
+------------------------------------------------------------------------
+
+-- GLOBALS: UnitGUID
+-- GLOBALS: UnitIsPlayer
+
+local function InspectUnit(guid, unit)
+	if guid or unit then
+		if guid then
+			unit = unit or MooUnit:GetUnitByGUID(guid)
+		else -- if unit then
+			guid = guid or UnitGUID(unit)
+		end
+	end
+	if UnitIsPlayer(unit) then
+		MooInspect:QueueInspect(guid)
+	end
+end
+
+function lib:InspectUnit(unit)
+	local guid = UnitGUID(unit)
+	if guid then
+		InspectUnit(guid, unit)
+	end
+end
+
+function lib:InspectRoster()
+	for guid, unit in MooUnit:IterateRoster() do
+		InspectUnit(guid, unit)
+	end
+end
---
+++ README.md
@@ -0,0 +1,175 @@
+Overview
+--------
+
+[MooSpec-1.0][project] is a library that provides information about player specializations and roles.
+
+__MooSpec-1.0__ will track the following changes to units within the group:
+
+* specialization
+* role as determined by class and specialization
+* role as assigned by the Dungeon Finder or as manually assigned in a raid group
+
+__MooSpec-1.0__ can also inspect player units that are outside of the group as long as they have a valid unit ID.
+
+
+API Methods
+-----------
+
+### [GetBlizzardRole](#getblizzardrole)
+
+Returns the assigned role for the GUID in a group formed via the Dungeon Finder, or the role manually assigned in a raid group.
+
+    blizzardRole = lib:GetBlizzardRole(guid)
+
+#### Arguments:
+
+* `guid` - string: [GUID][]
+
+#### Returns:
+
+* `blizzardRole` - string: `"TANK"`, `"HEALER"`, `"DAMAGER"`, `"NONE"`
+
+### [GetRole](#getrole)
+
+Returns the role for the GUID as determined by class and specialization.  This differentiates between melee and ranged DPS specializations.
+
+    role = lib:GetRole(guid)
+
+#### Arguments:
+
+* `guid` - string: [GUID][]
+
+#### Returns:
+
+* `role` - string: `"tank"`, `"healer"`, `"melee"`, `"ranged"`, `"none"`
+
+### GetSpecialization
+
+Returns the specialization for the GUID.
+
+    specialization, name = lib:GetSpecialization(guid)
+
+#### Arguments:
+
+* `guid` - string: [GUID][]
+
+#### Returns:
+
+* `specialization` - number: [specialization ID][]
+* `name` - string: the name of the specialization, e.g., `"blood`", `"protection"`, `"arcane"`, etc.
+
+### GetSpecializationName
+
+Returns the name of the specialization by ID.
+
+    name = lib:GetSpecializationName(specialization)
+
+#### Arguments:
+
+* `specialization` - number: [specialization ID][]
+
+#### Returns:
+
+* `name` - string: the name of the specialization, e.g., `"blood"`, `"protection"`, `"arcane"`, etc.
+
+### InspectUnit
+
+Queue a unit for asynchronous update of its specialization and role.
+
+    lib:InspectUnit(unit)
+
+#### Arguments:
+
+* `unit` - string: [unit ID][], e.g., `"player"`, `"target"`, `"raid15"`, etc.
+
+### InspectRoster
+
+Queue each member of the group for asynchronous update of their specialization and role.
+
+    lib:InspectRoster()
+
+### RegisterCallback
+
+Registers a function to handle the specified callback.
+
+    lib.RegisterCallback(handler, callback, method, arg)
+
+#### Arguments:
+
+* `handler` - table/string: your addon object or another table containing a function at `handler[method]`, or a string identifying your addon
+* `callback` - string: the name of the callback to be registered
+* `method` - string/function/nil: a key into the `handler` table, or a function to be called, or `nil` if `handler` is a table and a function exists at `handler[callback]`
+* `arg` - a value to be passed as the first argument to the callback function specified by `method`
+
+#### Notes:
+
+* If `handler` is a table, `method` is a string, and `handler[method]` is a function, then that function will be called with `handler` as its first argument, followed by the callback name and the callback-specific arguments.
+* If `handler` is a table, `method` is nil, and `handler[callback]` is a function, then that function will be called with `handler` as its first argument, followed by the callback name and the callback-specific arguments.
+* If `handler` is a string and `method` is a function, then that function will be called with the callback name as its first argument, followed by the callback-specific arguments.
+* If `arg` is non-nil, then it will be passed to the specified function. If `handler` is a table, then `arg` will be passed as the second argument, pushing the callback name to the third position. Otherwise, `arg` will be passed as the first argument.
+
+### UnregisterCallback
+
+Unregisters a specified callback.
+
+    lib.UnregisterCallback(handler, callback)
+
+#### Arguments:
+
+* `handler` - table/string: your addon object or a string identifying your addon
+* `callback` - string: the name of the callback to be unregistered
+
+
+Callbacks
+---------
+
+__MooSpec-1.0__ provides the following callbacks to notify interested addons.
+
+### MooSpec_UnitBlizzardRoleChanged
+
+Fires when the assigned role in a group formed via the Dungeon Finder or the role manually assigned in a raid group is changed.
+
+#### Arguments:
+
+* `guid` - string: [GUID][] of the unit whose role has changed
+* `unit` - string: [unit ID], e.g., `"player"`, `"target"`, `"raid15"`, etc.
+* `oldRole` - string: the previous assigned role, see [GetBlizzardRole](#getblizzardrole)
+* `newRole` - string: the current assigned role, see [GetBlizzardRole](#getblizzardrole)
+
+### MooSpec_UnitRoleChanged
+
+Fires when the role of a unit as determined by its class and specialization has changed.
+
+#### Arguments:
+
+* `guid` - string: [GUID][] of the unit whose role has changed
+* `unit` - string: [unit ID], e.g., `"player"`, `"target"`, `"raid15"`, etc.
+* `oldRole` - string: the previous role, see [GetRole](#getrole)
+* `newRole` - string: the current role, see [GetRole](#getrole)
+
+### MooSpec_UnitSpecializationChanged
+
+Fires when the specialization of a unit has changed.
+
+#### Arguments:
+
+* `guid` - string: [GUID][] of the unit whose specialization has changed
+* `unit` - string: [unit ID], e.g., `"player"`, `"target"`, `"raid15"`, etc.
+* `oldSpecialization` - number: the previous [specialization ID][]
+* `newSpecialization` - number: the current [specialization ID][]
+
+License
+-------
+__MooSpec-1.0__ is released under the 2-clause BSD license.
+
+
+Feedback
+--------
+
++ [Report a bug or suggest a feature][project-issue-tracker].
+
+  [project]: https://www.github.com/ultijlam/moospec-1-0
+  [project-issue-tracker]: https://github.com/ultijlam/moospec-1-0/issues
+  [GUID]: https://wow.gamepedia.org/GUID
+  [specialization ID]: https://wow.gamepedia.com/API_GetInspectSpecialization
+  [unit ID]: https://wow.gamepedia.com/UnitId
