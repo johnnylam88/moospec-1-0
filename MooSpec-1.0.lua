02bf1ac2dae55752a6ca5f166b49b38fa1d7d28c
Johnny C. Lam <jlam88@gmail.com>
2018-11-28 17:52:02 -0500

    Bump minor version to 5 for release.

--- MooSpec-1.0.lua
+++ MooSpec-1.0.lua
@@ -6,7 +6,7 @@
 -- GLOBALS: assert
 -- GLOBALS: LibStub
 
-local MAJOR, MINOR = "MooSpec-1.0", 4
+local MAJOR, MINOR = "MooSpec-1.0", 5
 assert(LibStub, MAJOR .. " requires LibStub")
 assert(LibStub("CallbackHandler-1.0", true), MAJOR .. " requires CallbackHandler-1.0")
 assert(LibStub("MooInspect-1.0", true), MAJOR .. " requires MooInspect-1.0")
